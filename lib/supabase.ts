bd3749a1226d8c27cfcdeffd60e35bcfba15778f
FABRETTI Florent <florent.fabretti@etu.univ-amu.fr>
2026-01-21 20:37:02 +0100

    - changement bdd sur bdd demo - changement icon apk

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Récupère ces valeurs dans ton tableau de bord Supabase (Settings > API)
-const supabaseUrl = 'https://lgsspvcxayanodmvgkzb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxnc3NwdmN4YXlhbm9kbXZna3piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMTI4MTksImV4cCI6MjA4Mzg4ODgxOX0.YNaDaUcRyjLnn0J8mN3Z3fCzNVH4iGWEJPwNc5rpGDw';
+const supabaseUrl = 'https://bnbebbcgkbaubpxwmemt.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuYmViYmNna2JhdWJweHdtZW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMjI3NjgsImV4cCI6MjA4NDU5ODc2OH0.HlkQ4YCd6Yn-dOfTemJXX6Nmx29Ojx2iQx2kAC-nBlk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
