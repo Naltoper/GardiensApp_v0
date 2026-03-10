8e60e126adb8bc32f43599582ae630281ed176a7
FABRETTI Florent <florent.fabretti@etu.univ-amu.fr>
2026-03-10 13:11:17 +0100

    - bare de navigateur web toujours fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,34 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet,ViewStyle } from 'react-native';
 
 export default function RootLayout() {
   const bgColor = '#cbe7e6c3'; // Définis ta couleur ici une seule fois
+  // On définit le style Web à part avec un cast "any" pour accepter le '100dvh'
+  // Cela évite l'erreur sur DimensionValue
+  const webStyle: any = Platform.OS === 'web' ? {
+    height: '100dvh',
+    width: '100%',
+    position: 'fixed',
+    overflow: 'hidden',
+  } : {};
 
   return (
     <SafeAreaProvider>
-      <StatusBar style="light" backgroundColor="black" translucent={false} />
+      {/* Barre d'état native */}
+      <StatusBar style="dark" backgroundColor="transparent" translucent={true} />
       
-      <SafeAreaView style={[styles.container, { backgroundColor: bgColor }]} edges={['top', 'bottom']}>
+      <SafeAreaView 
+        style={[styles.container, { backgroundColor: bgColor }, webStyle as ViewStyle]} 
+        edges={['top', 'bottom']}
+      >
         <Stack
           screenOptions={{
             headerStyle: { backgroundColor: '#000dbfff' },
             headerTintColor: '#fff',
             headerTitleStyle: { fontWeight: '800', fontSize: 18 },
             headerShadowVisible: false,
-            
-            
             contentStyle: { backgroundColor: bgColor }, 
           }}
         >
@@ -39,6 +49,5 @@ export default function RootLayout() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // Cette couleur ne se verra que si le Stack ne prend pas toute la place
   },
 });
